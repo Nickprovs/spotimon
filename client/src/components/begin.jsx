c00a3beb8e7b4c6a04386c1438fd4fdbe9e4ed54
Nickprovs <provost.nikolai@gmail.com>
2020-01-12 23:22:22 -0500

    playing with landing page.

--- client/src/components/begin.jsx
+++ client/src/components/begin.jsx
@@ -3,7 +3,16 @@ import React, { Component } from "react";
 class Begin extends Component {
   state = {};
   render() {
-    return <h1>Begin</h1>;
+    return (
+      <div>
+        <h1>Begin</h1>
+        <video width="100%" height="auto" id="preview" preload="true" autoPlay="true" loop="loop">
+          <source src="/Videos/preview.mp4" type="video/mp4" />
+          {/* <source src="myVideo.webm" type="video/webm" />
+          <source src="myVideo.ogv" type="video/ogg" /> */}
+        </video>
+      </div>
+    );
   }
 }
 
