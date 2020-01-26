59fed9af081bde77aad99c4dafc284ef04617545
Nickprovs <provost.nikolai@gmail.com>
2020-01-26 16:48:51 -0500

    The various public assets

---
+++ auth-server/authorization_code/public/precache-manifest.1760659d29731a8d3097aaab054a6945.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6fe14db42ceaf946abdcf8376dec73e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10c296bed4653ca02c61",
+    "url": "/static/css/main.78d4daae.chunk.css"
+  },
+  {
+    "revision": "2d434afd0371dcb30c94",
+    "url": "/static/js/2.45fa91a0.chunk.js"
+  },
+  {
+    "revision": "abf9dce7bfc91acc1f76f4a3096f8868",
+    "url": "/static/js/2.45fa91a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10c296bed4653ca02c61",
+    "url": "/static/js/main.4ec6f759.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "570a244cacd3d78b8c75ac5dd622f537",
+    "url": "/static/media/Montserrat-ExtraLight.570a244c.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  }
+]);
---
+++ auth-server/authorization_code/public/precache-manifest.d3ff7f442197c04680a9c343cdd788e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7c588aa23282bf1cbfa2bb79f49156",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea24fedae3f230ab66a1",
+    "url": "/static/css/main.78d4daae.chunk.css"
+  },
+  {
+    "revision": "5e01d4a341b420f82a4d",
+    "url": "/static/js/2.5adfbb0c.chunk.js"
+  },
+  {
+    "revision": "abf9dce7bfc91acc1f76f4a3096f8868",
+    "url": "/static/js/2.5adfbb0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea24fedae3f230ab66a1",
+    "url": "/static/js/main.3a9f821d.chunk.js"
+  },
+  {
+    "revision": "95366edfe6df4eccd374",
+    "url": "/static/js/runtime-main.9a259b97.js"
+  },
+  {
+    "revision": "570a244cacd3d78b8c75ac5dd622f537",
+    "url": "/static/media/Montserrat-ExtraLight.570a244c.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  }
+]);
