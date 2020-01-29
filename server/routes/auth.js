642a23673a3f382af89ba3fda37922f2f908f7dc
Nickprovs <provost.nikolai@gmail.com>
2020-01-29 17:05:23 -0500

    testttt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -12,16 +12,7 @@ const stateKey = "spotify_auth_state";
 console.log("server port", config.get("serverPort"));
 console.log("is server port equal to string 80: ", config.get("serverPort") === "80");
 
-const serverUriWithPort = `${config.get("serverAddress")}:${config.get("serverPort")}/api/auth/serverCallback`;
-const serverUriWithoutPort = `${config.get("serverAddress")}/api/auth/serverCallback`;
-const serverOnPort80 =
-  config.get("serverPort") === "80" ||
-  config.get("serverPort") === 80 ||
-  config.get("serverPort") === "8081" ||
-  config.get("serverPort") === 8081;
-let serverRedirectUri = "";
-if (serverOnPort80) serverRedirectUri = serverUriWithoutPort;
-else serverRedirectUri = serverUriWithPort;
+const serverRedirectUri = `${config.get("serverAddress")}:${config.get("serverPort")}/api/auth/serverCallback`;
 
 //serverOnPort80 ? serverUriWithoutPort : serverUriWithPort;
 
