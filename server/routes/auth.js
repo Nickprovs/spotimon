86f2374670e6dce84fac8bb8c096a60c2b305677
Nickprovs <provost.nikolai@gmail.com>
2020-01-29 14:47:11 -0500

    test

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -57,6 +57,7 @@ router.get("/login", function(req, res) {
 });
 
 router.get("/serverCallback", function(req, res) {
+  console.log("HEY");
   // your application requests refresh and access tokens
   // after checking the state parameter
 
